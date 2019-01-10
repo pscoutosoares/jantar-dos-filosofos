b3c1ac4d8a61013c2092027492819bb1092a1f23
Carlos A. Lopes <carlosaugusto4703@gmail.com>
2019-01-10 17:29:09 -0300

    threads completas

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
-#include <lpc2103.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <mutex.h>
+#include <pthread.h>
+#include <unistd.h>
+
 #define N_FILOSOFOS 5
 #define COMENDO 0
 #define PENSANDO 1 
 #define FOME 2
 
-
+pthread_t thread_filosofo[N_FILOSOFOS]; //instacias da struct das thread que serao iniciadas
 
 typedef struct Filosofo{
 	int index;
@@ -18,46 +20,69 @@ typedef struct Garfo{
 	int index;
 };
 
-Filosofo mesa[N_FILOSOFOS];
+Filosofo mesa[11];
 
-void criar_Filosofos(int n_Filosofos);
+void criar_Filosofos();
 void rodar_Filosofo(int index_filosofo);
-void tentar_pegar_garfos();
-void pegar_garfo_esq(int index_filosofo);
-void pegar_garfo_esq(int index_filosofo);
-void soltar_garfos(int index_filosofo);
+//void tentar_pegar_garfos();
+//void pegar_garfo_esq(int index_filosofo);
+//void pegar_garfo_dir(int index_filosofo);
+//void soltar_garfos(int index_filosofo);
 void comer ( int id_filosofo );
 void pensar ( int id_filosofo );
 
 
-void rodar_Filosofo(int index_filosofo){
-	int index_filosofo
+void *rodar_Filosofo(void *param){
+	int * index_filosofo = (int *) (param);
 	printf("Filosofo %d iniciado!\n", index_filosofo);
 	while(true){
 		pensar(index_filosofo);
-		tentar_pegar_garfos(index_filosofo);
+		//tentar_pegar_garfos(index_filosofo);
 		comer(index_filosofo);
-		soltar_garfos(index_filosofo);
+		//soltar_garfos(index_filosofo);
 	}
 }
 
 void criar_Filosofos(){
 	//index dos filosofos são os números impares do range(1,10)
-	int cont_aux = 0;
 	for (int i=1; i<=N_FILOSOFOS*2; i++){
 		if(i%2!=0){
 			Filosofo filosofo;  //criando filosofo
 			filosofo.index = i; 
-			mesa[cont_aux]=filosofo; //add filosofo no vetor de filosofos criados
+			mesa[i]=filosofo; //add filosofo no vetor de filosofos criados
 			printf("Filosofo %d criado!\n", i);
 			cont_aux++
 		}
 	}
 
 	//iniciando filosofos
-	for (int i=1; i<=N_FILOSOFOS; i++){
-		//THREAD AQUI
-		rodar_Filosofo(mesa[i].index);
-	}
+	pthread_create( &(thread_filosofo[0]), NULL, rodar_Filosofo, (void *) &(1) );
+	pthread_create( &(thread_filosofo[1]), NULL, rodar_Filosofo, (void *) &(3) );
+	pthread_create( &(thread_filosofo[2]), NULL, rodar_Filosofo, (void *) &(5) );
+	pthread_create( &(thread_filosofo[3]), NULL, rodar_Filosofo, (void *) &(7) );
+	pthread_create( &(thread_filosofo[4]), NULL, rodar_Filosofo, (void *) &(9) );
+
+}
+
+void comer(int index_filosofo){
+	//passa estado para comendo
+	mesa[index_filosofo].estado = COMENDO;
+	printf("Filosofo %d está COMENDO!\n", index_filosofo);
+	sleep(2);
+
+
+}
+
+void pensar(int index_filosofo){
+	//passa estado para comendo
+	mesa[index_filosofo].estado = PENSANDO;
+	printf("Filosofo %d está PENSANDO!\n", index_filosofo);
+	sleep(2);
+}
+
+void main(){
+	criar_Filosofos();
+
+
 
 }
